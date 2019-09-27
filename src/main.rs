0f0a8809ac7fa33704d203dfa2d720fed2f6f628
Pascal Seitz <pascal.seitz@gmail.com>
2019-09-27 18:50:39 +0200

    yep

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use serde_json::Deserializer;
-use toml::ser::Serializer;
+
+
+
+// fn main() {
+//     let mut toml = String::new();
+//     let mut deserializer = Deserializer::from_str(r#"{ "kana": {"ent_seq": "1000000"}, "kanji": [] }"#);
+//     let mut serializer = Serializer::pretty(&mut toml);
+//     serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
+// }
 
 fn main() {
-    println!("Hello, world!");
-    let mut toml = String::new();
-
-    let mut deserializer = Deserializer::from_str(r#"{ "kana": {"ent_seq": "1000000"}, "kanji": [] }"#);
-    let mut serializer = Serializer::pretty(&mut toml);
-    serde_transcode::transcode(&mut deserializer, &mut serializer).unwrap();
-
+    let val: serde_json::Value = serde_json::from_str(r#"{ "kana": {"ent_seq": "1000000"}, "kanji": [] }"#).unwrap();
+    toml::ser::to_string_pretty(&val).unwrap();
 }
